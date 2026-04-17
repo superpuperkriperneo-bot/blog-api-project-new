17be2da30e8cc38172d3a86c948b496239bf2add
Sultan <superpuperkriperneo@gmail.com>
2026-04-17 15:16:43 +0500

    practise | settings of database in settings.py changed

--- config/settings.py
+++ config/settings.py
@@ -105,9 +105,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER':os.environ.get('POSTGRES_USER'),
-        'PASSWORD':os.environ.get('POSTGRES_PASSWORD'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER':os.environ.get('DB_USER'),
+        'PASSWORD':os.environ.get('DB_PASSWORD'),
         'HOST':os.environ.get('DB_HOST'),
         'PORT':'5432',
     }
